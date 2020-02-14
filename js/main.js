f45bf86e47d37603958a4a1a2f1f35d6ffeb015f
Renan <renan.oliveira267@etecia.com.br>
2020-02-13 21:26:39 -0300

    Commit 1

--- js/main.js
+++ js/main.js
@@ -1,33 +1,40 @@
-alert("Welcome!")
+//alert("Welcome!")
 //console.log("Save")
 //console.warn("Virus instalado com sucesso")
 //console.error("Falha ao atualizar")
+
+
+let pacientes = document.querySelectorAll(".paciente")
+
 var titulo = document.querySelector("#titulo");
 titulo.textContent = "ETECIA Nutrição e Dietética"
 
-//pegar peso
-let paciente = document.querySelector(".paciente")
-let tdPeso = paciente.querySelector(".info-peso")
-let peso = tdPeso.textContent
-//pegar altura
-let tdAltura = paciente.querySelector(".info-altura")
-let altura = tdAltura.textContent
+for (var i = 0; i < pacientes.length; i++) {
+    let paciente = pacientes[i]
+    //pegar peso
+    let tdPeso = paciente.querySelector(".info-peso")
+    let peso = tdPeso.textContent
+    //pegar altura
+    let tdAltura = paciente.querySelector(".info-altura")
+    let altura = tdAltura.textContent
 
-let pesoValido = true;
-let alturaValido = true;
-//validar dados
-if (peso <= 0 || peso > 1000) {
-    alert("Peso Inválido")
-    pesoValido = false;
-}
-if (altura <= 0 || altura > 3.00) {
-    alert("Altura Inválido")
-    alturaValido = false;
-}
-if (pesoValido && alturaValido) {
-    //calcular imc
-    let imc = peso / altura ** 2
-    //escrever o IMC da pessoa
-    let tdImc = paciente.querySelector(".info-imc")
-    tdImc.textContent = imc;
+    let pesoValido = true;
+    let alturaValido = true;
+    //validar dados
+    if (peso <= 0 || peso > 1000) {
+        alert("Peso Inválido")
+        pesoValido = false;
+        
+    }
+    if (altura <= 0 || altura > 3.00) {
+        alert("Altura Inválido")
+        alturaValido = false;
+    }
+    if (pesoValido && alturaValido) {
+        //calcular imc
+        let imc = peso / altura ** 2
+        //escrever o IMC da pessoa
+        let tdImc = paciente.querySelector(".info-imc")
+        tdImc.textContent = imc.toFixed(2);
+    }
 }
