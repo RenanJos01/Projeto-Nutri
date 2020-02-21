8e62498748b8406bb9d07b188a948277dd746d82
ruanpablo <ruan.jesus@etec.sp.gov.br>
2020-02-20 22:19:16 -0300

    Criação de uns negocio ai

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var titulo = document.querySelector("#titulo");
+let titulo = document.querySelector("#titulo");
 titulo.textContent = "ETECIA Nutrição e Dietética"
 
 let pacientes = document.querySelectorAll(".paciente")
@@ -27,3 +27,59 @@ pacientes.forEach(function (paciente) {
         tdImc.textContent = imc.toFixed(2);
     }
 });
+
+titulo.addEventListener("click", mostrarMensagem);
+titulo.addEventListener("click", trocarCor);
+
+function mostrarMensagem() {
+    console.log("Evento aconteceu.")
+}
+function trocarCor() {
+    let cor = parseInt(Math.random() * 255);
+    titulo.style.color = `rgb(${cor},${cor},${cor})`;
+}
+
+let botao = document.querySelector("#botao-cadastrar")
+botao.addEventListener("click", function (evento) {
+    evento.preventDefault();
+    //pegar valores do form
+    let formulario = document.querySelector("#form-paciente");
+    let nome = formulario.nome.value;
+    let peso = formulario.peso.value;
+    let altura = formulario.altura.value;
+    let gordura = formulario.gordura.value;
+    //criar linha
+    let table = document.querySelector("#tabela-pacientes");
+    let tr = document.createElement("tr");
+    tr.classList.add("paciente");
+    table.appendChild(tr);
+    //criar td
+    //nome
+    let tdNome = document.createElement("td");
+    tdNome.textContent = nome;
+    tr.appendChild(tdNome);
+    tdNome.classList.add("info-nome")
+    //peso
+    let tdPeso = document.createElement("td");
+    tdPeso.textContent = peso;
+    tr.appendChild(tdPeso);
+    tdPeso.classList.add("info-peso")
+    //altura
+    let tdAltura = document.createElement("td");
+    tdAltura.textContent = altura;
+    tr.appendChild(tdAltura);
+    tdAltura.classList.add("info-altura")
+    //gordura
+    let tdGordura = document.createElement("td");
+    tdGordura.textContent = gordura;
+    tr.appendChild(tdGordura);
+    tdGordura.classList.add("info-gordura")
+    //IMCZADA
+    let tdImc = document.createElement("td");
+    //tdImc =;
+    tr.appendChild(tdImc);
+    tdImc.classList.add("info-imc")
+    //console.log(tr);
+    
+});
+
